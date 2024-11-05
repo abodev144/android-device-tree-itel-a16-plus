0a78fca45c04630eddb45ab8da0918ad77040ce6
melek saidani <MBCMELEK3@HOTMAIL.COM>
2024-11-05 09:35:37 -0800

    Update and rename omni_itel-A16-Plus.mk to twrp_itel-A16-Plus.mk

--- twrp_itel-A16-Plus.mk
+++ twrp_itel-A16-Plus.mk
@@ -8,14 +8,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from itel-A16-Plus device
 $(call inherit-product, device/itel/itel-A16-Plus/device.mk)
 
 PRODUCT_DEVICE := itel-A16-Plus
-PRODUCT_NAME := omni_itel-A16-Plus
+PRODUCT_NAME := twrp_itel-A16-Plus
 PRODUCT_BRAND := Itel
 PRODUCT_MODEL := itel A16 Plus
 PRODUCT_MANUFACTURER := itel

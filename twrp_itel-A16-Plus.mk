ed22d9855728aa6c4d047d6e61ee2bdfb41d07d1
melek saidani <MBCMELEK3@HOTMAIL.COM>
2024-11-05 10:13:59 -0800

    Update twrp_itel-A16-Plus.mk

--- twrp_itel-A16-Plus.mk
+++ twrp_itel-A16-Plus.mk
@@ -5,9 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
@@ -23,6 +20,5 @@ PRODUCT_MANUFACTURER := itel
 PRODUCT_GMS_CLIENTID_BASE := android-transsion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="A16Plus-SA332Q-10-OP-V022-20220704"
-
-BUILD_FINGERPRINT := Itel/SA332Q/itel-A16-Plus:10/QP1A.190711.020/OP-V022-20220704:user/release-keys
+    PRIVATE_BUILD_DESC="A16Plus-SA332Q-10-OP-V022-20220704" \
+    BUILD_FINGERPRINT="Itel/SA332Q/itel-A16-Plus:10/QP1A.190711.020/OP-V022-20220704:user/release-keys"

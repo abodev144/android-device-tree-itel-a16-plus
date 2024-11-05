ce54c7391910334ef71ae32a235b07e76d4cc0db
melek saidani <MBCMELEK3@HOTMAIL.COM>
2024-11-05 09:32:14 -0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_itel-A16-Plus-user
-add_lunch_combo omni_itel-A16-Plus-userdebug
-add_lunch_combo omni_itel-A16-Plus-eng
+add_lunch_combo twrp_itel-A16-Plus-user
+add_lunch_combo twrp_itel-A16-Plus-userdebug
+add_lunch_combo twrp_itel-A16-Plus-eng

9f81c44eef064101c1998d4ac66a04370b5ed0f0
melek saidani <MBCMELEK3@HOTMAIL.COM>
2024-11-05 10:10:32 -0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,4 @@
+# AndroidProducts.mk
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
@@ -8,7 +9,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_itel-A16-Plus.mk
 
-COMMON_LUNCH_CHOICES := \
+COMMON_LUNCH_CHOICES += \
     twrp_itel-A16-Plus-user \
     twrp_itel-A16-Plus-userdebug \
     twrp_itel-A16-Plus-eng
